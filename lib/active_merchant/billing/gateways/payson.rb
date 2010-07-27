73b5347965e441ce326a186be295e2344a7f66d0
Jonas Grimfelt <grimen@gmail.com>
2010-07-27 15:17:27 +0200

    Refactored the initial version a bit, and added support for PaymentDetails.

--- lib/active_merchant/billing/gateways/payson.rb
+++ lib/active_merchant/billing/gateways/payson.rb
@@ -3,104 +3,122 @@ require 'active_merchant/billing/gateways/payson/payson_response'
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class PaysonGateway < Gateway
-      REDIRECT_URL = 'https://www.payson.se/PaySecure'
-      ENDPOINT_URL = 'https://api.payson.se/1.0/Pay/'
-      
-      # The countries the gateway supports merchants from as 2 digit ISO country codes
-      self.supported_countries = ['SE']
-      
-      # The homepage URL of the gateway
-      self.homepage_url = 'https://api.payson.se/'
-      
+      ENDPOINT_URLS = {
+        :production => {
+          :authorize => 'https://api.payson.se/1.0/Pay/',
+          :pay => 'https://www.payson.se/PaySecure/',
+          :validate => 'https://api.payson.se/1.0/Validate/',
+          :payment_details => 'https://api.payson.se/1.0/PaymentDetails/'
+        }
+      }
+
       # The name of the gateway
       self.display_name = 'Payson'
-      
+      # The homepage URL of the gateway
+      self.homepage_url = 'https://api.payson.se/'
+      # The countries the gateway supports merchants from as 2 digit ISO country codes
+      self.supported_countries = ['SE']
+      # The default currency for the transactions if no currency is provided
+      self.default_currency = 'SEK'
+      # The format of the amounts used by the gateway
+      # :dollars => '12.50'
+      # :cents => '1250'
+      self.money_format = :dollars
+
       def initialize(options = {})
         requires!(options, :login, :password)
         @options = options
         super
-      end  
-      
+      end
+
       def authorize(options = {})
-        post = {}
-        add_receivers(post, options)
-        add_customer(post, options)
-        add_callbacks(post, options)
-        add_meta(post, options)
-        
-        response = commit('authorize', post)
-        @token = response.token if response.success?
+        @post = ActiveMerchant::PostData.new
+
+        self.add_receivers(options)
+        self.add_customer(options)
+        self.add_callbacks(options)
+        self.add_meta(options)
+
+        response = commit('authorize')
+        @token = response.success? ? response.token : nil
         response
       end
-      
-      def payment_redirection_url
-        "#{REDIRECT_URL}?token=#{@token}"
+
+      def payment_redirection_url(token = nil)
+        endpoint = endpoint_url('pay')
+        token = token || @token
+        "#{endpoint}?token=#{token}"
       end
 
-      private                       
-      
-      def add_receivers(post, options)
-        requires!(options, :receivers)
-        options[:receivers].each_with_index do |v, i|
-          post["receiverList.receiver(#{i}).email"] = v[:email]
-          post["receiverList.receiver(#{i}).amount"] = v[:amount]
+      def payment_details(token = nil)
+        endpoint = endpoint_url(:payment_details)
+        token = token || @token
+        url = "#{endpoint}?token=#{token}"
+
+        response_body = ssl_request(:get, url, nil)
+        ActiveMerchant::Billing::PaysonResponse.new(self.parse(response_body))
+      end
+
+      protected
+
+        def add_receivers(options)
+          requires!(options, :receivers)
+          options[:receivers].each_with_index do |v, i|
+            @post["receiverList.receiver(#{i}).email"] = v[:email]
+            @post["receiverList.receiver(#{i}).amount"] = v[:amount]
+          end
         end
-      end
 
-      def add_customer(post, options)
-        requires!(options, :sender)
-        post.merge!({'senderEmail' => options[:sender][:email],
-            'senderFirstName' => options[:sender][:first_name],
-            'senderLastName' => options[:sender][:last_name] })
-      end
-
-      def add_callbacks(post, options)
-        requires!(options, :return_url, :cancel_url)
-        post.merge!({'returnUrl' => options[:return_url],
-            'cancelUrl' => options[:cancel_url]})
-      end
-      
-      def add_meta(post, options)
-        requires!(options, :memo)
-        options[:currency_code] ||= "SEK"
-        post.merge!({ 'custom' => options[:custom],
-            'memo' => options[:memo],
-            'currencyCode' => options[:currency_code]})
-      end
-
-      def headers(options)
-        { 'PAYSON-SECURITY-USERID' => options[:login],
-          'PAYSON-SECURITY-PASSWORD' => options[:password]}
-      end
-
-      def parse(body)
-        response_slice = body.split('&')
-        response_hash = {}
-
-        response_slice.each do |rs|
-          rs_split = rs.split('=')
-          response_hash[rs_split[0]] = rs_split[1]
+        def add_customer(options)
+          requires!(options, :sender)
+          @post.merge!('senderEmail' => options[:sender][:email],
+                      'senderFirstName' => options[:sender][:first_name],
+                      'senderLastName' => options[:sender][:last_name])
         end
-        ActiveMerchant::Billing::PaysonResponse.new response_hash
-      end
 
-      def endpoint_url(action)
-        case action
-        when "authorize"
-          ENDPOINT_URL
-        when "pay"
-          REDIRECT_URL
+        def add_callbacks(options)
+          requires!(options, :return_url, :cancel_url)
+          @post.merge!('returnUrl' => options[:return_url],
+                      'cancelUrl' => options[:cancel_url])
         end
-      end
-      
-      def commit(action, parameters)
-        parse( ssl_post(endpoint_url(action), post_data(parameters), headers(@options)) )
-      end
 
-      def post_data(parameters)
-        parameters.collect { |key, value| "#{key}=#{CGI.escape(value.to_s)}" }.join("&")
-      end
+        def add_meta(options)
+          requires!(options, :memo)
+          options[:currency_code] ||= self.default_currency
+          @post.merge!('custom' => options[:custom],
+                      'memo' => options[:memo],
+                      'currencyCode' => options[:currency_code])
+        end
+
+        def headers(options)
+          {'PAYSON-SECURITY-USERID' => options[:login],
+           'PAYSON-SECURITY-PASSWORD' => options[:password]}
+        end
+
+        def parse(body)
+          return {} if body.blank?
+
+          body.split('&').inject({}) do |memo, chunk|
+            next if chunk.empty?
+            key, value = chunk.split('=', 2)
+            next if key.empty?
+            value = value.nil? ? nil : CGI.unescape(value)
+            memo[CGI.unescape(key)] = value
+            memo
+          end
+        end
+
+        def endpoint_url(action)
+          ENDPOINT_URLS[self.gateway_mode][action.to_sym]
+        rescue
+          ENDPOINT_URLS[:production][action.to_sym]
+        end
+
+        def commit(action, post = ActiveMerchant::PostData.new)
+          @post.merge!(post)
+          response_body = ssl_post(endpoint_url(action), @post.to_post_data, headers(@options))
+          ActiveMerchant::Billing::PaysonResponse.new(self.parse(response_body))
+        end
     end
   end
 end
-
--- test/unit/gateways/payson_test.rb
+++ test/unit/gateways/payson_test.rb
@@ -9,21 +9,21 @@ class PaysonTest < Test::Unit::TestCase
 
     @credit_card = credit_card
     @amount = 100
-    
-    @options = { 
+
+    @options = {
       :order_id => '1',
       :billing_address => address,
       :description => 'Store Purchase'
     }
   end
-  
+
   def test_successful_purchase
     @gateway.expects(:ssl_post).returns(successful_purchase_response)
-    
+
     assert response = @gateway.purchase(@amount, @credit_card, @options)
-    assert_instance_of 
+    assert_instance_of
     assert_success response
-    
+
     # Replace with authorization number from the successful response
     assert_equal '', response.authorization
     assert response.test?
@@ -31,19 +31,37 @@ class PaysonTest < Test::Unit::TestCase
 
   def test_unsuccessful_request
     @gateway.expects(:ssl_post).returns(failed_purchase_response)
-    
+
     assert response = @gateway.purchase(@amount, @credit_card, @options)
     assert_failure response
     assert response.test?
   end
 
-  private
-  
-  # Place raw successful response from gateway here
-  def successful_purchase_response
+  protected
+
+  def successful_authorization
+<<-RESPONSE
+RESPONSE
   end
-  
-  # Place raw failed response from gateway here
-  def failed_purcahse_response
+
+  def failed_authorization
+<<-RESPONSE
+RESPONSE
   end
+
+  def successful_payment_details
+<<-HELLO
+HELLO
+  end
+
+  def failed_payment_details
+<<-RESPONSE
+errorList.error(0).errorId  ::  520003
+errorList.error(0).message  ::  Authentication failed; Credentials were not valid
+responseEnvelope.ack  ::  FAILURE
+responseEnvelope.timestamp  ::  2010-07-27T15:05:22
+responseEnvelope.version  ::  1.0
+RESPONSE
+  end
+
 end
